4eb17e13a75844000ff1922086dc5169f5f14ddb
unknown <ing.dev.cristian@gmail.com>
2025-04-17 20:49:42 -0500

    configuracion de yaml y se agrego imagen logo

---
+++ lib/core/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// En este archivo se crean y nombran colores personalizados para posteriormente llamarlos por el nombre de la varible y el archivo AppColors
+class AppColors {
+  static const Color primaryColor = Color(0xFFCCCCCC);
+  static const Color secondaryColor = Color(0xFFF35E48);
+  static const Color thirdColor = Color(0xFF000000);
+  static const Color fourthColor = Color(0xFFFFFFFF);
+}
